e2df4348b17a6b54c6fe4130ef3b787ba142843d
andre <lauralme96@gmail.com>
2025-09-18 18:14:25 -0500

    se ingreso DB logica 2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,6 +3,6 @@ INSERT INTO categoria (id, nombre) VALUES (2, 'Fuentes');
 INSERT INTO categoria (id, nombre) VALUES (3, 'Refrigeracion');
 INSERT INTO categoria (id, nombre) VALUES (4, 'Teclados/Mouses');
 INSERT INTO categoria (id, nombre) VALUES (5, 'Monitores');
-INSERT INTO rol (id, tipo) VALUES (1, 'COSTUMER');
-INSERT INTO rol (id, tipo) VALUES (2, 'ADMIN');
+INSERT INTO rol (id, tipo) VALUES (1, 'costumer');
+INSERT INTO rol (id, tipo) VALUES (2, 'admin');
 INSERT INTO marca (id, nombre) VALUES (1, 'Asus');"
