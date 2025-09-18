3698259e9789097ecf759d7c1057f83b04a0a314
andre <lauralme96@gmail.com>
2025-09-18 18:02:23 -0500

    se ingreso DB logica

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,3 +3,5 @@ INSERT INTO categoria (id, nombre) VALUES (2, 'Fuentes');
 INSERT INTO categoria (id, nombre) VALUES (3, 'Refrigeracion');
 INSERT INTO categoria (id, nombre) VALUES (4, 'Teclados/Mouses');
 INSERT INTO categoria (id, nombre) VALUES (5, 'Monitores');
+
+INSERT INTO rol (id, tipo) VALUES (1, 'COSTUMER');
